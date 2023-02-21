cd0fc484a20c8e5e18ecbda488ed256d989f5671
SahilMishraDev <msahil76640@gmail.com>
2023-02-21 11:03:06 +0530

    changes

--- services/payment-services.ts
+++ services/payment-services.ts
@@ -1,19 +1,19 @@
 import stripe from "stripe";
 
 const stripeClient = new stripe(
-    process.env.STRIPE_SECRET_KEY as any,
-    undefined as any
+  process.env.STRIPE_SECRET_KEY as any,
+  undefined as any
 );
 
 export const createCheckoutSessionService = async (line_items: any) => {
-    const session = await stripeClient.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items,
-        mode: "payment",
-        success_url: `${process.env.CLIENT_URL}/order/success`,
-        cancel_url: `${process.env.CLIENT_URL}/cart`,
-    });
-    return session;
+  const session = await stripeClient.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items,
+    mode: "payment",
+    success_url: `${process.env.CLIENT_URL}/order/success`,
+    cancel_url: `${process.env.CLIENT_URL}/cart`,
+  });
+  return session;
 };
 
 // export const createCheckoutSessionService = async ({ amount, id }: any) => {
